74abce797f81c8f2cd66e318247e69c7d98a8f8c
torisutanjon <giraotristan@gmail.com>
2023-12-09 23:16:12 +0800

    update: edited projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -15,56 +15,56 @@ const Projects = () => {
       </p>
       <Project
         index={0}
-        image={icons.tesla}
-        repo="https://github.com/torisutanjon/tesla-website-clone"
-        demo="https://tristanjohngirao-teslawebsiteclone.netlify.app/"
-        title="Tesla Website Clone"
-        description="Trying to clone the Tesla Website."
-        techs={[icons.vite, icons.react, icons.typescript, icons.tailwind]}
-      />
-      <Project
-        index={1}
-        image={icons.spacex}
-        repo="https://github.com/torisutanjon/spacex_website_clone"
-        demo="https://tristanjohngirao-spacexwebsiteclone.vercel.app/"
-        title="SpaceX Website Clone"
-        description="Trying to clone the Spacex Website."
-        techs={[icons.vite, icons.react, icons.typescript, icons.tailwind]}
-      />
-      <Project
-        index={2}
-        image={icons.starlink}
-        repo="https://github.com/torisutanjon/starlink-website-clone"
-        demo="https://tristanjohngirao-starlinkwebsiteclone.vercel.app/"
-        title="Starlink Website Clone"
-        description="Trying to clone the Starlink Website."
-        techs={[icons.vite, icons.react, icons.typescript, icons.tailwind]}
-      />
-      <Project
-        index={3}
-        image={icons.todoApp}
-        repo="https://github.com/torisutanjon/todo-app-t3"
-        demo="https://tristanjohngirao-todoappt3.vercel.app/"
-        title="Todo Application"
-        description="Project created to learn the newly released T3 Stack."
+        image={icons.chatApp}
+        repo="https://github.com/torisutanjon/updated-chat-app"
+        title="Chat Application"
+        description="A chat application created using T3 stack. Utilizing the capability of the Socket.io to make a bi-directional communication between a client and a server."
         techs={[
-          isDark ? icons.darkNext : icons.lightNext,
           icons.t3Stack,
+          icons.expressJS,
+          icons.nodejs,
           icons.mongodb,
           icons.typescript,
           icons.tailwind,
         ]}
       />
       <Project
+        index={1}
+        image={icons.tesla}
+        repo="https://github.com/torisutanjon/tesla-website-clone"
+        demo="https://tristanjohngirao-teslawebsiteclone.netlify.app/"
+        title="Tesla Website Clone"
+        description="Tesla website homepage front-end clone."
+        techs={[icons.vite, icons.react, icons.typescript, icons.tailwind]}
+      />
+      <Project
+        index={2}
+        image={icons.spacex}
+        repo="https://github.com/torisutanjon/spacex_website_clone"
+        demo="https://tristanjohngirao-spacexwebsiteclone.vercel.app/"
+        title="SpaceX Website Clone"
+        description="Tesla website homepage front-end clone."
+        techs={[icons.vite, icons.react, icons.typescript, icons.tailwind]}
+      />
+      <Project
+        index={3}
+        image={icons.starlink}
+        repo="https://github.com/torisutanjon/starlink-website-clone"
+        demo="https://tristanjohngirao-starlinkwebsiteclone.vercel.app/"
+        title="Starlink Website Clone"
+        description="Starlink website homepage front-end clone."
+        techs={[icons.vite, icons.react, icons.typescript, icons.tailwind]}
+      />
+      <Project
         index={4}
-        image={icons.chatApp}
-        repo="https://github.com/torisutanjon/updated-chat-app"
-        title="Chat Application"
-        description="A chat application utilizing capability of Socket.io to make a  make bi-directional communication between a client and the server."
+        image={icons.todoApp}
+        repo="https://github.com/torisutanjon/todo-app-t3"
+        demo="https://tristanjohngirao-todoappt3.vercel.app/"
+        title="Todo Application"
+        description="An online todo-app project created using T3 stack."
         techs={[
           isDark ? icons.darkNext : icons.lightNext,
-          icons.expressJS,
-          icons.nodejs,
+          icons.t3Stack,
           icons.mongodb,
           icons.typescript,
           icons.tailwind,
